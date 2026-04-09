4c6a1a6fa7af19c5b61f47e778b32f577acc704f
Gabriel Pelincel <gabrielpelincel@gmail.com>
2026-04-09 20:24:46 -0300

    Base do projeto

---
+++ src/test/java/com/teacompanion/TEACompanion_API/TeaCompanionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teacompanion.TEACompanion_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaCompanionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
