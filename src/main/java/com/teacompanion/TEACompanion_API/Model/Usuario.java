63ff127c5e0831293b62b6faac9fd9c937959ae7
Gabriel Pelincel <gabrielpelincel@gmail.com>
2026-04-10 20:32:56 -0300

    Feat: Crud de usuários

--- src/main/java/com/teacompanion/TEACompanion_API/Model/Usuario.java
+++ src/main/java/com/teacompanion/TEACompanion_API/Model/Usuario.java
@@ -1,6 +1,7 @@
 package com.teacompanion.TEACompanion_API.Model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -8,6 +9,7 @@ import lombok.Data;
 @Entity
 public class Usuario {
     @Id
+    @GeneratedValue
     protected Integer id_usuario;
     private String username;
     private String password;
