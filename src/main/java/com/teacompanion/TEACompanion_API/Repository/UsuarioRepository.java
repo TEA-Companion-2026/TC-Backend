46682badf0b726842a4b9ac1339b7d2aeee1dfdd
Gabriel Pelincel <gabrielpelincel@gmail.com>
2026-04-30 20:05:56 -0300

    Fix: Correção de merges

--- src/main/java/com/teacompanion/TEACompanion_API/Repository/UsuarioRepository.java
+++ src/main/java/com/teacompanion/TEACompanion_API/Repository/UsuarioRepository.java
@@ -1,5 +1,9 @@
 package com.teacompanion.TEACompanion_API.Repository;
 
-public class UsuarioRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.teacompanion.TEACompanion_API.Model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Usuario findByEmail(String email);
 }
