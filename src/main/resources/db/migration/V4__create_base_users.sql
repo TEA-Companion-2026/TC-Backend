f618a643bdbffcc460f65a7b9fae59e8725fb546
Gabriel Pelincel <gabrielpelincel@gmail.com>
2026-05-20 18:42:06 -0300

    Feat: Criação de usuários base

---
+++ src/main/resources/db/migration/V4__create_base_users.sql
@@ -0,0 +1,9 @@
+INSERT INTO usuario
+(nivel_acesso, password, nome, email)
+VALUES('RESPONSAVEL','$2a$10$zg34UxAZ5m3hjLqjJSPdT.irhW7EyGA46EjW052XJYsTjmz4BDX0a', 'Gabriel Pelincel Ramalho', 'resp1@teste.com');
+INSERT INTO usuario
+(nivel_acesso, password, nome, email)
+VALUES('RESPONSAVEL','$2a$10$zg34UxAZ5m3hjLqjJSPdT.irhW7EyGA46EjW052XJYsTjmz4BDX0a', 'Gabriel Pelincel Ramalho', 'resp2@teste.com');
+INSERT INTO usuario
+(nivel_acesso, password, nome, email)
+VALUES('PSICOLOGO', '$2a$10$zg34UxAZ5m3hjLqjJSPdT.irhW7EyGA46EjW052XJYsTjmz4BDX0a', 'Gabriel Pelincel Ramalho', 'psi@teste.com');
