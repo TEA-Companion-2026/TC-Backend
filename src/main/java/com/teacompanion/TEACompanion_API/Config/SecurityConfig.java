542726b06993325630e427d7ff5570a928f9f020
Gabriel Pelincel <gabrielpelincel@gmail.com>
2026-05-20 09:23:40 -0300

    Feat: Cors config

--- src/main/java/com/teacompanion/TEACompanion_API/Config/SecurityConfig.java
+++ src/main/java/com/teacompanion/TEACompanion_API/Config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.teacompanion.TEACompanion_API.Config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.teacompanion.TEACompanion_API.Service.Auth.SecurityFilter;
 
@@ -26,16 +31,18 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http.csrf(csrf -> csrf.disable())
         .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(
             req -> {
               req.requestMatchers(
                   "/api/auth/login",
+                  "/api/usuario",
                   "/v3/api-docs",
                   "/v3/api-docs/**",
                   "/swagger-ui/**",
                   "/swagger-ui.html",
-                  "/error"
-              ).permitAll();
+                  "/error").permitAll();
 
               req.anyRequest().authenticated();
             })
@@ -52,5 +59,27 @@ public class SecurityConfig {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    configuration.setAllowedOrigins(List.of(
+        "http://localhost:5173",
+        "http://localhost:8081",
+        "http://localhost:4200"));
+
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+    configuration.setAllowedHeaders(List.of("*"));
+
+    // 4. (Opcional) Necessário se o front for mandar cookies
+    // configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+
+    return source;
   }
 }
